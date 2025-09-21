7da4daca897b04dd60aa4fdc504774666c5cf2f1
Abdul Ghani <ghanisheikh26@gmail.com>
2025-09-22 02:18:07 +0530

    setup documents page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import RegisterPage from "./pages/RegisterPage";
 import FileUpload from "./pages/FileUpload";
+import Documents from "./pages/Documents";
 
 const App=()=>{
   return(
     <Routes>
       <Route path="/upload-file" element={<FileUpload />}></Route>
+      <Route path="/documents" element={<Documents />}></Route>
       <Route path="/login-page" element={<LoginPage />}></Route>
       <Route path="/" element={<Navigate to="/login-page"/>}></Route>
       <Route path="/signup-page" element={<RegisterPage />}></Route>
