030d946ff7cfe283ae051e6ae02cafd9d21713ea
Abdul Ghani <ghanisheikh26@gmail.com>
2025-11-17 16:00:35 +0530

    added start command

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use("/auth",authRouter);
 app.use("/user",userRouter);
 app.use("/file",fileRouter);
-
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running at ${process.env.PORT}`);
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`server is running at ${PORT}`);
 });
